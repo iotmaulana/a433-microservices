9373f7e70096c2078c558803033c63fe7669c6e8
iotmaulana <140522454+iotmaulana@users.noreply.github.com>
2023-11-06 10:00:44 +0700

    Create build and push shell command

---
+++ build_push_image_shipping.sh
@@ -0,0 +1,10 @@
+# build docker image
+docker build -t ghcr.io/iotmaulana/shipping_service:latest .
+
+# login to GitHub Packages
+# CR_PAT is GitHub access token(classic)
+# please run `export CR_PAT=YOUR_TOKEN`
+echo $CR_PAT | docker login ghcr.io -u iotmaulana --password-stdin
+
+# push image to GitHub Packages
+docker push ghcr.io/iotmaulana/shipping_service:latest
